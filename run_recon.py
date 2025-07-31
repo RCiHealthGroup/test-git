8dd200b67250d5fcac63640be06296de44a22e65
Pablo Pino <pdpinog@gmail.com>
2025-07-31 19:02:15 -0400

    Add dummy scripts as examples

---
+++ run_recon.py
@@ -0,0 +1,11 @@
+def load_twix():
+  print("Loading twix...")
+
+
+def run_reconstruction():
+  print("Running reconstruction...")
+
+
+if __name__ == "__main__":
+  load_twix()
+  run_reconstruction()
---
+++ train_model.py
@@ -0,0 +1,11 @@
+def load_data():
+  print("Loading data...")
+
+
+def train_model():
+  print("Training model...")
+
+
+if __name__ == "__main__":
+  load_data()
+  train_model()
